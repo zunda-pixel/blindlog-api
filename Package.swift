ff6393a4ae0d37f468d0fa0ebc0f9dc924c91344
zunda.dev@gmail.com <zunda.dev@gmail.com>
2025-08-31 12:32:53 +0900

    fix platform

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version: 6.2
+// swift-tools-version: 6.1
 
 import PackageDescription
 
 let package = Package(
   name: "BlindLogServer",
   platforms: [
-    .macOS(.v26),
+    .macOS(.v15),
   ],
   products: [
     .executable(
