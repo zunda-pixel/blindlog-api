018c5df02c80d5ce0ae5130d7a5cd1668cb3ca91
zunda.dev@gmail.com <zunda.dev@gmail.com>
2025-09-12 03:35:54 +0900

    format

--- Sources/App/App.swift
+++ Sources/App/App.swift
@@ -5,10 +5,10 @@ import Hummingbird
 struct AppCommand: AsyncParsableCommand, AppArguments {
   @Option(name: .shortAndLong)
   var hostname: String = "127.0.0.1"
-  
+
   @Option(name: .shortAndLong)
   var port: Int = 8080
-  
+
   func run() async throws {
     let app = try await buildApplication(self)
     try await app.runService()
