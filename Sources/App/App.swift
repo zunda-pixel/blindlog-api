8c11d94af4ad8e9f0b632be801972d9c1b4860bd
zunda.dev@gmail.com <zunda.dev@gmail.com>
2025-09-12 03:32:22 +0900

    fix

--- Sources/App/App.swift
+++ Sources/App/App.swift
@@ -1,26 +1,17 @@
 import ArgumentParser
 import Hummingbird
-import Logging
-import NIOCore
-import NIOPosix
-import Valkey
 
 @main
-struct App: AsyncParsableCommand, AppArguments {
+struct AppCommand: AsyncParsableCommand, AppArguments {
   @Option(name: .shortAndLong)
   var hostname: String = "127.0.0.1"
-
+  
   @Option(name: .shortAndLong)
   var port: Int = 8080
-
+  
   func run() async throws {
     let app = try await buildApplication(self)
-    do {
-      try await app.runService()
-    } catch {
-      app.logger.error("\(error.localizedDescription)")
-      throw error
-    }
+    try await app.runService()
   }
 }
 
