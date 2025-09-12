ebb84a170e79dfb242dcea5f61092379068467ed
zunda.dev@gmail.com <zunda.dev@gmail.com>
2025-09-12 09:57:46 +0900

    remove database

--- Sources/App/BuildApplication.swift
+++ Sources/App/BuildApplication.swift
@@ -27,40 +27,40 @@ func buildApplication(
     logger: Logger(label: "Valkey")
   )
 
-  let config = PostgresClient.Configuration(
-    host: environment.get("POSTGRES_HOSTNAME")!,
-    username: environment.get("POSTGRES_USER")!,
-    password: environment.get("POSTGRES_PASSWORD")!,
-    database: environment.get("POSTGRES_DB")!,
-    tls: .disable
-  )
-
-  let databaseClient = PostgresClient(
-    configuration: config,
-    backgroundLogger: Logger(label: "PosgresClient")
-  )
-
-  let migrations = DatabaseMigrations()
-
-  let database = await PostgresPersistDriver(
-    client: databaseClient,
-    migrations: migrations,
-    logger: Logger(label: "Postgres")
-  )
-
+//  let config = PostgresClient.Configuration(
+//    host: environment.get("POSTGRES_HOSTNAME")!,
+//    username: environment.get("POSTGRES_USER")!,
+//    password: environment.get("POSTGRES_PASSWORD")!,
+//    database: environment.get("POSTGRES_DB")!,
+//    tls: .disable
+//  )
+//
+//  let databaseClient = PostgresClient(
+//    configuration: config,
+//    backgroundLogger: Logger(label: "PosgresClient")
+//  )
+//
+//  let migrations = DatabaseMigrations()
+//
+//  let database = await PostgresPersistDriver(
+//    client: databaseClient,
+//    migrations: migrations,
+//    logger: Logger(label: "Postgres")
+//  )
+//
   let router = Router()
-  router.addRoutes(
-    UsersRouter(cache: cache, database: databaseClient).build(),
-    atPath: "users"
-  )
-  router.addRoutes(
-    MeRouter(cache: cache, database: databaseClient).build(),
-    atPath: "me"
-  )
-  router.addRoutes(
-    SignupRouter(cache: cache, database: databaseClient).build(),
-  )
-
+//  router.addRoutes(
+//    UsersRouter(cache: cache, database: databaseClient).build(),
+//    atPath: "users"
+//  )
+//  router.addRoutes(
+//    MeRouter(cache: cache, database: databaseClient).build(),
+//    atPath: "me"
+//  )
+//  router.addRoutes(
+//    SignupRouter(cache: cache, database: databaseClient).build(),
+//  )
+//
   router.addRoutes(
     AppleAppSiteAssosiationRouter(
       appIds: [environment.get("APPLE_APP_ID")!]
@@ -74,20 +74,20 @@ func buildApplication(
     ),
     services: [
       cache,
-      databaseClient,
-      database,
+//      databaseClient,
+//      database,
     ],
     logger: Logger(label: "Server")
   )
 
-  app.beforeServerStarts {
-    try await migrations.apply(
-      client: databaseClient,
-      groups: [.persist],
-      logger: Logger(label: "Postgres Migrations"),
-      dryRun: false
-    )
-  }
+//  app.beforeServerStarts {
+//    try await migrations.apply(
+//      client: databaseClient,
+//      groups: [.persist],
+//      logger: Logger(label: "Postgres Migrations"),
+//      dryRun: false
+//    )
+//  }
 
   return app
 }
