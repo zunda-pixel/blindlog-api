8f3d3ab4821b58a426e6dd72de134262203cd816
zunda.dev@gmail.com <zunda.dev@gmail.com>
2025-09-19 21:25:13 +0900

    add package

--- Package.swift
+++ Package.swift
@@ -22,7 +22,8 @@ let package = Package(
       from: "1.0.0-rc.1"
     ),
     .package(url: "https://github.com/vapor/postgres-kit.git", from: "2.0.0"),
-    .package(url: "https://github.com/valkey-io/valkey-swift.git", from: "0.1.0"),
+    .package(url: "https://github.com/valkey-io/valkey-swift.git", from: "0.2.0"),
+    .package(url: "https://github.com/zunda-pixel/swift-webauthn.git", branch: "add-init"),
   ],
   targets: [
     .executableTarget(
@@ -33,6 +34,7 @@ let package = Package(
         .product(name: "HummingbirdPostgres", package: "hummingbird-postgres"),
         .product(name: "PostgresKit", package: "postgres-kit"),
         .product(name: "Valkey", package: "valkey-swift"),
+        .product(name: "WebAuthn", package: "swift-webauthn"),
       ]
     ),
     .testTarget(
