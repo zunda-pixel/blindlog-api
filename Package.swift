ba569eadc73a58c6833a952f4a16293e5b65d533
zunda.dev@gmail.com <zunda.dev@gmail.com>
2025-09-25 00:29:35 +0900

    add Records Library

--- Package.swift
+++ Package.swift
@@ -32,6 +32,8 @@ let package = Package(
     .package(url: "https://github.com/swift-server/swift-openapi-hummingbird.git", from: "2.0.0"),
     .package(url: "https://github.com/apple/swift-openapi-generator.git", from: "1.10.0"),
     .package(url: "https://github.com/vapor/jwt-kit.git", from: "5.0.0"),
+    .package(url: "https://github.com/coenttb/swift-records.git", from: "0.0.1"),
+    .package(url: "https://github.com/pointfreeco/swift-tagged.git", from: "0.10.0"),
   ],
   targets: [
     .executableTarget(
@@ -46,6 +48,8 @@ let package = Package(
         .product(name: "WebAuthn", package: "swift-webauthn"),
         .product(name: "OpenAPIHummingbird", package: "swift-openapi-hummingbird"),
         .product(name: "JWTKit", package: "jwt-kit"),
+        .product(name: "Records", package: "swift-records"),
+        .product(name: "Tagged", package: "swift-tagged"),
       ],
       plugins: [
         .plugin(name: "OpenAPIGenerator", package: "swift-openapi-generator")
