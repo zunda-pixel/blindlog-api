53a6ae926dbe6e1aed1c35eb347106d370f6a0e2
zunda.dev@gmail.com <zunda.dev@gmail.com>
2025-10-01 22:52:08 +0900

    format

--- Sources/App/BuildApplication.swift
+++ Sources/App/BuildApplication.swift
@@ -104,7 +104,7 @@ func buildApplication(
       challengeGenerator: .init {
         // https://www.w3.org/TR/webauthn-3/#sctn-appid-exclude-extension
         // challenge parameter 32 random bytes
-        Array(Data(AES.GCM.Nonce())) + Array(Data(AES.GCM.Nonce())) + Array(Data(AES.GCM.Nonce())) // 36 bytes
+        Array(Data(AES.GCM.Nonce())) + Array(Data(AES.GCM.Nonce())) + Array(Data(AES.GCM.Nonce()))  // 36 bytes
       }
     ),
     appleAppSiteAssociation: .init(
