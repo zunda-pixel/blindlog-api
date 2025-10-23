46fe6f6562b792922088ad2458233dc4e96b5bf0
zunda <47569369+zunda-pixel@users.noreply.github.com>
2025-10-24 00:27:50 +0900

    Enhance database schema with citext and indexing
    Added citext extension, created index for passkey_credentials, and modified user_email table to use citext for email and added ON DELETE CASCADE.

--- Database/init/create.sql
+++ Database/init/create.sql
@@ -1,3 +1,5 @@
+CREATE EXTENSION IF NOT EXISTS citext;
+
 CREATE TABLE public.users (
   id uuid NOT NULL,
   CONSTRAINT users_pk PRIMARY KEY (id)
@@ -12,9 +14,12 @@ CREATE TABLE public.passkey_credentials (
   CONSTRAINT passkey_credentials_user_fk FOREIGN KEY (user_id) REFERENCES public.users (id)
 );
 
+CREATE INDEX passkey_credentials_user_id_idx ON public.passkey_credentials(user_id);
+
 CREATE TABLE public.user_email (
   user_id uuid NOT NULL,
-  email varchar NOT NULL,
+  email citext NOT NULL,
+  CONSTRAINT user_email_pk PRIMARY KEY (user_id, email),
   CONSTRAINT user_email_email_key UNIQUE (email),
-  CONSTRAINT user_email_user_fk FOREIGN KEY (user_id) REFERENCES public.users (id)
+  CONSTRAINT user_email_user_fk FOREIGN KEY (user_id) REFERENCES public.users (id) ON DELETE CASCADE
 );
